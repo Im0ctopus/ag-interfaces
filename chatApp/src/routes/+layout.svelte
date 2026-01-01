1fb7fff7637ba62b52e0fbe362bf8fc80b656ec2
0 <leoferreira1712@gmail.com>
2026-01-01 16:48:11 +0000

    feat: initialize chatApp project

---
+++ chatApp/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import './layout.css'
+  import favicon from '$lib/assets/favicon.svg'
+
+  let { children } = $props()
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<main
+  class="antialiased min-h-screen relative flex gap-0 justify-start items-start"
+>
+  {@render children()}
+</main>
